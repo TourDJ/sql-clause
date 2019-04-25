8ae64ba7dd515df93643786fd5476d0a5faad478
Hank <tangxiaojun_123@126.com>
2019-04-25 21:41:45 +0800

    Update python-mongo.py

--- specific/python-mongo.py
+++ specific/python-mongo.py
@@ -1,6 +1,10 @@
 
-## connect
+## connect monogdb
 from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-client = MongoClient('mongodb://localhost:27017/')
+
+#client = MongoClient('localhost', 27017)
+#client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient('mongodb://tang:123456@127.0.0.1:27017/?authSource=tangdb&authMechanism=SCRAM-SHA-256')
 db = client.test_database
+collection = db.test_collection
+
